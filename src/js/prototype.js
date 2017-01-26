bea10646004ae2bd3aef6e77d01c182edd642e3c
Francis C. Rupert <francisrupert@gmail.com>
2017-01-25 20:06:35 -0600

    marker combinator: copies markup

--- src/js/prototype.js
+++ src/js/prototype.js
@@ -59,3 +59,50 @@ $('body').on('change', '[data-behavior~="select-multi-all"]', function(event) {
   }
 
 })
+
+function markerDemoMarkup() {
+
+  var $source = $('#pcp-marker-demo__target');
+  var $target = $('#pcp-marker-demo__markup');
+
+  var $sourceContent = $source.parent().html();
+
+  // far too lazy and repetitive, but i don't feel like DRYing this
+  $sourceContent = $sourceContent.replace(new RegExp('<','g'), '&lt;');
+  $sourceContent = $sourceContent.replace(new RegExp('>','g'), '&gt;');
+  $sourceContent = $sourceContent.replace(new RegExp('                ','g'), '');
+  $sourceContent = $sourceContent.replace(' id="pcp-marker-demo__target"', '');
+  $sourceContent = $sourceContent.replace(' pcp-marker--barFoo', '');
+  $sourceContent = $sourceContent.replace(' pcp-marker--fooBar', '');
+  $sourceContent = $sourceContent.replace(' pcp-marker--loremIpsum', '');
+  $sourceContent = $sourceContent.replace(' style="position: absolute; left: 210px; top: 100px;"', '');
+  $sourceContent = $sourceContent.replace(' none', '');
+
+  $sourceContent = $sourceContent.replace(' pcp-marker--modified', ' <strong style="color:black;">pcp-marker--modified</strong>');
+  $sourceContent = $sourceContent.replace(' pcp-marker--low', ' <strong style="color:black;">pcp-marker--low</strong>');
+  $sourceContent = $sourceContent.replace(' pcp-marker--active', ' <strong style="color:black;">pcp-marker--active</strong>');
+  $sourceContent = $sourceContent.replace(' pcp-marker--impacted', ' <strong style="color:black;">pcp-marker--impacted</strong>');
+  $sourceContent = $sourceContent.replace(' pcp-marker--high', ' <strong style="color:black;">pcp-marker--high</strong>');
+
+  console.log('$sourceContent is ' + $sourceContent);
+
+  $target.html($sourceContent);
+
+}
+
+function markerDemo() {
+  $('body').on('change', '[data-behavior~="markerDemo"]', function() {
+
+    var $self = $(this);
+    var $target = $('#pcp-marker-demo__target');
+    $target
+      .removeClass($self.attr('data-marker-other'))
+      .addClass($self.val())
+    ;
+
+    markerDemoMarkup();
+
+  });
+}
+
+markerDemo();
